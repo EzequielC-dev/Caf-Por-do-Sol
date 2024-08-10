932608bb15ae099fe07d3ba8b6c902a8435753f4
EzequielC-Dev <ezequiel34324l@gmail.com>
2024-08-10 20:34:20 -0300

    correção menu responsivo

---
+++ README.md
@@ -0,0 +1,1 @@
+# Caf-Por-do-Sol
---
+++ js/script.js
@@ -0,0 +1,39 @@
+if (window.innerWidth > 300) {
+  function navMenu() {
+    const accordList = document.querySelector(".cabecalho-navegacao");
+    const burgerCheckbox = document.getElementById("burger-checkbox");
+    if (window.innerWidth > 1050) {
+      accordList.classList.add("ativo");
+    }
+
+    function activeAccordion() {
+      if (burgerCheckbox.checked) {
+        accordList.classList.add("ativo");
+      } else {
+        accordList.classList.remove("ativo");
+      }
+    }
+    burgerCheckbox.addEventListener("change", activeAccordion);
+  }
+  navMenu();
+}
+
+function animacaoScroll() {
+  const elementosScroll = document.querySelectorAll(".scroll");
+
+  if (elementosScroll.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      elementosScroll.forEach((elemento) => {
+        const pegarTop = elemento.getBoundingClientRect().top;
+        const sessaoVisivel = pegarTop - windowMetade < 0;
+        if (sessaoVisivel) {
+          elemento.classList.add("ativo");
+        }
+      });
+    }
+  }
+  window.addEventListener("scroll", animaScroll);
+}
+animacaoScroll();
